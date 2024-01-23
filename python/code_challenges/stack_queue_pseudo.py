5fefe0678845c819d5ec266ebcb4ef01a0f7247b
neginkou <negin.kou@gmail.com>
2024-01-22 18:33:48 -0800

    Implement PseudoQueue using two stacks for enqueue and dequeue operations/updated readme

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,22 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stack1 = Stack()  # For enqueue operations
+        self.stack2 = Stack()  # For dequeue operations
+
+    def enqueue(self, value):
+        # To enqueue, simply push the value onto stack1
+        self.stack1.push(value)
+
+    def dequeue(self):
+        if self.stack2.is_empty():
+            if self.stack1.is_empty():
+                return None  # PseudoQueue is empty
+            else:
+                # Transfer elements from stack1 to stack2 to reverse the order
+                while not self.stack1.is_empty():
+                    self.stack2.push(self.stack1.pop())
+
+        # Pop the top element from stack2 (which represents the front of the queue)
+        return self.stack2.pop()
---
+++ python/docs/stack_queue_pseudo/README.md
@@ -0,0 +1,15 @@
+# Challenge Title
+
+## Stack and Queue
+
+## Whiteboard Process
+
+![WhiteBoard](WhiteBoard11.png)
+
+## Approach & Efficiency
+
+The approach for the PseudoQueue class uses two stacks to simulate a queue. Enqueueing takes O(1) time and space, while dequeuing takes O(n) time (amortized) and O(n) space in the worst case due to transferring elements between the stacks. Overall, it offers a reasonable balance of time and space efficiency for queue operations.
+
+## Solution
+
+[stack_queue_pseudo](/python/code_challenges/stack_queue_pseudo.py)
