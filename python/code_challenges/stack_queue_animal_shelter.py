cc596989c986758faefdaaddb88adc6e69cecf26
neginkou <negin.kou@gmail.com>
2024-01-23 20:11:14 -0800

    added enqueue and dequeue functions/updated readme

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,12 +2,35 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
 
+    def __init__(self):
+        self.dog_queue = Queue()
+        self.cat_queue = Queue()
+
+    def enqueue(self, animal):
+        if animal.type == "cat":
+            self.cat_queue.enqueue(animal)
+        elif animal.type == "dog":
+            self.dog_queue.enqueue(animal)
+
+    def dequeue(self, pref):
+        if pref == "cat":
+            new_pet = self.cat_queue.dequeue()
+        elif pref == "dog":
+            new_pet = self.dog_queue.dequeue()
+        else:
+            return None
+
+        return new_pet
 
 class Dog:
-    pass
+
+    def __init__(self):
+        self.type = "dog"
+
 
 
 class Cat:
-    pass
+
+    def __init__(self):
+        self.type = "cat"
---
+++ python/docs/stack_queue_animal_shelter/README.md
@@ -0,0 +1,21 @@
+# Challenge Title
+
+## Stack and Queue Animal Shelter
+
+## Whiteboard Process
+
+![WhiteBoard](WhiteBoard12.png)
+
+## Approach & Efficiency
+
+The code uses two queues (one for dogs and one for cats) to manage an animal shelter.
+When animals are enqueued, they are placed in the respective queues based on their type.
+When dequeuing, animals are retrieved according to preference ("cat" or "dog").
+Efficiency:
+Enqueue: O(1) time and space complexity.
+Dequeue: O(1) time and space complexity.
+The code is efficient, with both enqueue and dequeue operations taking constant time and space.
+
+## Solution
+
+[stack_queue_animal_shelter](/python/code_challenges/stack_queue_animal_shelter.py)
